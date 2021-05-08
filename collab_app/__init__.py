a691417011fdf06247e84a17d5227b5cf612f8ce
joshuaparker99 <77903425+joshuaparker99@users.noreply.github.com>
2021-05-07 23:03:43 -0400

    wow

--- collab_app/__init__.py
+++ collab_app/__init__.py
@@ -2,10 +2,9 @@ import os
 from flask import Flask
 
 app = Flask(__name__, instance_relative_config=True)
-print("out collab app")
+print(__name__)
 def create_app(test_config=None):
     # create and configure the app
-    print("in collab app")
 
     app.config.from_mapping(
         SECRET_KEY='dev',
