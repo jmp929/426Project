425d8d2833bfe8c1c5a97d8230996d66574548dc
joshuaparker99 <77903425+joshuaparker99@users.noreply.github.com>
2021-05-07 23:24:49 -0400

    wow

--- collab_app/__init__.py
+++ collab_app/__init__.py
@@ -36,4 +36,4 @@ def create_app(test_config=None):
 
 
 create_app()
-app.run(port=4004, debug=config.DEBUG, host='0.0.0.0', use_reloader=False)
+app.run(port=4004, host='0.0.0.0', use_reloader=False)
